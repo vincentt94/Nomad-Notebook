e000d6825bb0f9314655f48aaef0e3770caa197c
mbauer99621 <bauermm9@augsburg.edu>
2025-03-14 14:11:21 -0500

    updated files

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+
+export const ADD_USER = gql`
+  mutation Mutation($input: UserInput!) {
+  addUser(input: $input) {
+    user {
+      username
+      _id
+    }
+    token
+  }
+}
+`;
