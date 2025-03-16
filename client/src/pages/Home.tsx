52de6072f163eabe9f47f3dbcdc95da0a79fb750
Kayla E <kengelstad16@gmail.com>
2025-03-16 17:13:15 -0500

    basic form structure

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <button>
+                <Link to="/createStory">
+                    Temp Create Story
+                </Link>
+            </button>
         </div>
     );
 }
