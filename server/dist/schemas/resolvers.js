0b38a1c519c2177c59c3c484f64dd85b03f47314
Kayla E <kengelstad16@gmail.com>
2025-03-17 00:11:16 -0500

    models and starter queries

--- server/dist/schemas/resolvers.js
+++ server/dist/schemas/resolvers.js
@@ -1,8 +1,33 @@
+import Story from "../models/Story.js";
+import User from "../models/User.js";
+import { signToken } from '../utils/auth.js';
 const resolvers = {
     Query: {
         hello: async () => {
             return "Hello World";
-        }
+        },
+        getStories: async () => {
+            return await Story.find().sort({ createdAt: -1 });
+        },
+        getUserStories: async (_, { userId }) => {
+            return await Story.find({ userId }).sort({ createdAt: -1 });
+        },
+        getUsers: async () => {
+            return await User.find().sort({ createdAt: -1 });
+        },
+    },
+    Mutation: {
+        addUser: async (_, { input }) => {
+            const newUser = await User.create({ ...input });
+            console.log(newUser);
+            const token = signToken(newUser.username, newUser._id);
+            return { token, newUser };
+        },
+        addStory: async (_, { title, story, imageUrl, userId }) => {
+            const newStory = new Story({ title, story, imageUrl, userId });
+            await newStory.save();
+            return newStory;
+        },
     }
 };
 export default resolvers;
--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,8 +1,56 @@
+import Story from "../models/Story.js";
+import User from "../models/User.js";
+import { signToken } from '../utils/auth.js'
+
+interface GetUserStoriesArgs {
+    userId: string
+}
+
+interface AddUserArgs {
+    input:{
+        username: string,
+        password: string
+    }
+}
+
+interface AddStoryArgs {
+    title: string,
+    story: string,
+    imageUrl: string,
+    userId: string
+}
+
 const resolvers = {
     Query: {
         hello: async () => {
             return "Hello World";
-        }
+        },
+
+        getStories: async () => {
+            return await Story.find().sort({ createdAt: -1 });
+        },
+
+        getUserStories: async (_: unknown, { userId }: GetUserStoriesArgs) => {
+            return await Story.find({ userId }).sort({ createdAt: -1 });
+        },
+
+        getUsers: async () => {
+            return await User.find().sort({ createdAt: -1 });
+        },
+    },
+
+    Mutation: {
+        addUser: async (_: unknown, { input }: AddUserArgs) => {
+            const newUser = await User.create({...input});
+            console.log(newUser);
+            const token = signToken(newUser.username, newUser._id);
+            return { token, newUser };
+        },
+        addStory: async (_: unknown, { title, story, imageUrl, userId }: AddStoryArgs) => {
+            const newStory = new Story({ title, story, imageUrl, userId });
+            await newStory.save();
+            return newStory;
+        },
     }
 }
 
