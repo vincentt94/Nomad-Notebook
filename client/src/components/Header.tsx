a76132dc4833327cb1a599a662134dbb44560172
vincentt94 <vincent.h.thao@gmail.com>
2025-03-17 13:37:34 -0500

    removed unnecessary code in login, signup, & header files

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,37 +12,36 @@ const Header = () => {
 
 
  return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+    <header>
+      <div>
         <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">Travel Journal</h1>
+          <Link to="/">
+            <h1 >Travel Journal</h1>
           </Link>
-          <p className="m-0">Please login or sign up to begin your journal!</p>
+          <p >Please login or sign up to begin your journal!</p>
         </div>
         <div>
           {/* Checking if the user is logged in to conditionally render profile link and logout button */}
           {Auth.loggedIn() ? (
             <>
-            {/* no /me route so need to remove that linked our area */}
-              <Link className="btn btn-lg btn-info m-2" to="/me">
-                {/* Retrieving the logged-in user's profile to display the username */}
-                {Auth.getProfile().data.username}'s profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              
+                {/* &nbsp; adds a space */}
+                Logged in as:&nbsp;
+                {Auth.getProfile().data.username}&nbsp;
+              <button onClick={logout}>
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
+              <Link to="/login">
+                Login&nbsp; 
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
+              <Link to="/signup">
+                Signup&nbsp;
               </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/mystories">
-                My Stories
+              <Link to="/mystories">
+                My Stories&nbsp;
               </Link>
             </>
           )}
