a2b95629247c438acd90489dff315a33a9df1496
mbauer99621 <bauermm9@augsburg.edu>
2025-03-17 11:21:25 -0500

    updated my stories, create story, and journal card

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -41,6 +41,9 @@ const Header = () => {
               <Link className="btn btn-lg btn-light m-2" to="/signup">
                 Signup
               </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/mystories">
+                My Stories
+              </Link>
             </>
           )}
         </div>
