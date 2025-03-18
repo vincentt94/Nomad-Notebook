6368aff9712b089fe140756f1c70adce9d786b78
vincentt94 <vincent.h.thao@gmail.com>
2025-03-18 11:21:50 -0500

    updated home page so it shows a feed of stories (randomly now) instead of just one randomly generated story

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,54 +1,57 @@
 import { Link } from 'react-router-dom';
-import { type MouseEvent} from 'react';
+import { type MouseEvent } from 'react';
 import Auth from '../utils/auth';
-import logo from '../assets/logo.png'; 
+import logo from '../assets/logo.png';
 
 
 
 const Header = () => {
-    const logout = (event: MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault();
-      // Logs the user out by calling the logout method from Auth
-      Auth.logout();
-    };
+  const logout = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    // Logs the user out by calling the logout method from Auth
+    Auth.logout();
+  };
 
 
   return (
     <header>
       <div>
         <div>
-        <div className="header-content">
+          <div className="header-content">
             <Link to="/" id="logoImg">
-                <img src={logo} alt="Travel Journal Logo"/>
+              <img src={logo} alt="Travel Journal Logo" />
             </Link>
             <Link to="/">
-                <h1>Nomad Notebook</h1>
+              <h1>Nomad Notebook</h1>
             </Link>
-        </div>
+          </div>
 
-        <p >Please login or sign up to begin your journal!</p>
+          <p >Please login or sign up to begin your journal!</p>
         </div>
         <div>
           {/* Checking if the user is logged in to conditionally render profile link and logout button */}
           {Auth.loggedIn() ? (
             <>
+              <Link to="/">
+                Home&nbsp;
+              </Link>
               <Link to="/mystories">
                 My Stories&nbsp;
               </Link>
               <button onClick={logout}>
                 Logout
               </button>
+
             </>
           ) : (
             <>
               <Link to="/login">
-                Login&nbsp; 
+                Login&nbsp;
               </Link>
-              
+
               <Link to="/signup">
                 Signup&nbsp;
               </Link>
-
 
             </>
           )}
