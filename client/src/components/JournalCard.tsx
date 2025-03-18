58167b7c31ddc9a36e61d1fc9ac454a0876ec4a2
Kayla E <kengelstad16@gmail.com>
2025-03-18 11:06:14 -0500

    make username optional for JournalCard

--- client/src/components/JournalCard.tsx
+++ client/src/components/JournalCard.tsx
@@ -5,7 +5,7 @@ interface JournalCardProps {
     title: string;
     story: string;
     image?: string;
-    username: string;
+    username?: string;
 }
 
 export default function JournalCard({ title, story, image, username }: JournalCardProps) {
@@ -14,7 +14,9 @@ export default function JournalCard({ title, story, image, username }: JournalCa
             {image && <img src={image} alt={title} className="journal-image" />}
             <div className="journal-content">
                 <h2 className="journal-title">{title}</h2>
-                <p className = "journal-author"> By: {username}</p>
+                {username && 
+                    <p className = "journal-author"> By: {username}</p>
+                }
                 <p className="journal-text">{story}</p>
             </div>
         </div>
