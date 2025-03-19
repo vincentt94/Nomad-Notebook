c5acd700a35434af7e2abc31ba189758b8fc5c57
vincentt94 <vincent.h.thao@gmail.com>
2025-03-19 13:54:59 -0500

    saving

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -8,7 +8,7 @@ import Story from "../models/Story.js";
 dotenv.config();
 
 //connects to MongoDB
-mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/travel-journal")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/travel-journal")
 
 //sample users
 const users = [
